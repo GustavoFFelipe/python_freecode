0acaec061dc4e203d5e829172ca30f7e2b91c3cf
Gustavo Felipe <gustavo.ffelipe19@gmail.com>
2020-08-09 09:13:07 -0300

    calculadora de loop

---
+++ loopcalc.py
@@ -0,0 +1,10 @@
+number = input("Enter with a number")
+sub = input('Enter with a number to subtration')
+number1= float(number)
+sub1 = float(sub)
+while number1 > sub1:
+    print(number1)
+    number1 = number1 - sub1
+
+print('Done')
+print('Your result is', number1)
