a914765d9194481fcb45b0ac4e2cba5fcceaadee
Gustavo Felipe <gustavo.ffelipe19@gmail.com>
2020-08-10 20:28:58 -0300

    def and return

--- loop.py
+++ loop.py
@@ -4,3 +4,27 @@ while True:
         break
         print(line)
         print('Done')
+
+x=5
+print('5 is the begin')
+def history():
+    print('Another history to tell.')
+    print('Another count to make.')
+    #return "Another day"
+
+x1 = 5 + 3
+print('If we plus 3 in 5.')
+print('We have', x1)
+#hirtory()
+
+#def number(double):
+    #if double > 8:
+    #    print("Double is more")
+#    elif double = 8:
+        #print("Everything is equal")
+    #else:
+        #print('Yo this is less')
+
+#number(15)
+#number(3)
+#number(8)
